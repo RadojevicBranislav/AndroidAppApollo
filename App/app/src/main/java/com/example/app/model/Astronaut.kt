97dce6bc1a354adc403dea3a2be5f11f753af9bb
Branislav Radojevic <branislavradojevicit4@gmail.com>
2022-11-20 13:11:53 +0100

    added model classes for Astronaut and DataSource

---
+++ App/app/src/main/java/com/example/app/model/Astronaut.kt
@@ -0,0 +1,5 @@
+package com.example.app.model
+
+data class Astronaut(public val name: String,public val craft: String) {
+
+}
---
+++ App/app/src/main/java/com/example/app/model/DataSource.kt
@@ -0,0 +1,5 @@
+package com.example.app.model
+
+data class DataSource (val message: String, val people: List<Astronaut>, val number: Int){
+
+}

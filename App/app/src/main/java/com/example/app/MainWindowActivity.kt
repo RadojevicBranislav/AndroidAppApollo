93b66c1b8bb503719d99e3af0f9d671289498b88
Bane <branislavradojevicit4@gmail.com>
2022-12-02 03:46:46 +0100

    Added drawables and bottom bar

--- App/app/src/main/java/com/example/app/MainWindowActivity.kt
+++ App/app/src/main/java/com/example/app/MainWindowActivity.kt
@@ -23,47 +23,47 @@ class MainWindowActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_window)
         var astronauts: List<Astronaut> = arrayListOf<Astronaut>()
-        apiCall()
+//        apiCall()
     }
 
-    private fun apiCall(){
-
-        val url = "http://api.open-notify.org/astros.json"
-
-
-
-        val queue = Volley.newRequestQueue(this)
-
-        
-
-        val jsonObjectRequest = JsonObjectRequest(
-            Request.Method.GET,url, null,
-            Response.Listener {response->
-                //this method for our api call successful
-            try {
-                var dataJson: JSONArray= response.getJSONArray("people")
-                for(i in 0..dataJson.length()){
-                    var dataObj: JSONObject = dataJson.getJSONObject(i)
-                    var name: String = dataObj.getString("name")
-                    var craft: String = dataObj.getString("craft")
-                    astronauts += Astronaut(name, craft)
-
-                    Toast.makeText(this, "Fetch success", Toast.LENGTH_SHORT)
-                    txtView.text = "Radi mamicu mu jebem"
-                }
-            } catch (e: JSONException){
-                Toast.makeText(this, "Fetching failed", Toast.LENGTH_SHORT)
-                txtView.text = "NE Radi mamicu mu jebem"
-            }
-
-
-            }, Response.ErrorListener {
-                //this is for api fail
-                Toast.makeText(this, "API failed", Toast.LENGTH_SHORT)
-                txtView.text = "Radi mamicu mu jebem ErrorLis"
-            }
-        )
-        //set json request to queue
-        queue.add(jsonObjectRequest)
-        }
+//    private fun apiCall(){
+//
+//        val url = "http://api.open-notify.org/astros.json"
+//
+//
+//
+//        val queue = Volley.newRequestQueue(this)
+//
+//
+//
+//        val jsonObjectRequest = JsonObjectRequest(
+//            Request.Method.GET,url, null,
+//            Response.Listener {response->
+//                //this method for our api call successful
+//            try {
+//                var dataJson: JSONArray= response.getJSONArray("people")
+//                for(i in 0..dataJson.length()){
+//                    var dataObj: JSONObject = dataJson.getJSONObject(i)
+//                    var name: String = dataObj.getString("name")
+//                    var craft: String = dataObj.getString("craft")
+//                    astronauts += Astronaut(name, craft)
+//
+//                    Toast.makeText(this, "Fetch success", Toast.LENGTH_SHORT)
+//
+//                }
+//            } catch (e: JSONException){
+//                Toast.makeText(this, "Fetching failed", Toast.LENGTH_SHORT)
+//
+//            }
+//
+//
+//            }, Response.ErrorListener {
+//                //this is for api fail
+//                Toast.makeText(this, "API failed", Toast.LENGTH_SHORT)
+//
+//            }
+//        )
+//        //set json request to queue
+//        queue.add(jsonObjectRequest)
+//        }
 }
